5123165a441b5727224fd6598dd9f08cc286351d
Lin Wang <wanglinseven@gmail.com>
2020-12-08 12:58:12 -0800

    Optimize 2-entry result by sorting first.

--- src/bin/day001.rs
+++ src/bin/day001.rs
@@ -2,8 +2,9 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::vec::Vec;
 use std::str::FromStr;
+use advent_of_code_2020::*;
 
-fn main() -> std::io::Result<()> {
+fn main() -> io::Result<()> {
     let input_file = File::open("src/bin/day001.input")?;
     let lines = io::BufReader::new(input_file).lines().map(|l| l.unwrap());
 
@@ -15,15 +16,25 @@ fn main() -> std::io::Result<()> {
 
     let mut res = 0;
 
-    for i in &entries[..entries.len() - 1] {
-        for j in &entries[1..] {
-            if i + j == 2020 {
-                res = i * j;
-                break;
-            }
+    bubble_sort(&mut entries);
+    let mut head_i = 0;
+    let mut tail_i = entries.len() - 1;
+    loop {
+        if head_i > tail_i {
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "No 2 entries summed to 2020."));
+        }
+
+        let sum = entries[head_i] + entries[tail_i];
+        if sum < 2020 {
+            head_i += 1;
+        } else if sum > 2020 {
+            tail_i -= 1;
+        } else {
+            res = entries[head_i] * entries[tail_i];
+            break;
         }
     }
-    println!("result: {}", res);
+    println!("sort first, 2-entry result: {}", res);
 
     for i in &entries[..entries.len() - 2] {
         for j in &entries[1..entries.len() - 1] {
@@ -35,7 +46,7 @@ fn main() -> std::io::Result<()> {
             }
         }
     }
-    println!("result: {}", res);
+    println!("3-entry result: {}", res);
 
     return Ok(());
 }
