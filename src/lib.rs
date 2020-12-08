53830f9340052742efc68b2faf1444721a934db3
Lin Wang <wanglinseven@gmail.com>
2020-12-08 12:57:35 -0800

    Add bubble sort.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::vec::Vec;
+
+pub fn bubble_sort(v: &mut Vec<i32>) {
+    loop {
+        let mut swapped = false;
+        for i in 0..(v.len() - 1) {
+            if v[i] > v[i+1] {
+                v.swap(i, i+1);
+                swapped = true;
+            }
+        }
+        if swapped == false {
+            break;
+        }
+    }
 }
