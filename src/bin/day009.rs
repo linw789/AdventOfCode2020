d62f37aab80f8424f0f1b63ff56bb658555c41b6
Lin Wang <wanglinseven@gmail.com>
2020-12-25 21:18:08 -0800

    Day 09, part 2.

--- src/bin/day009.rs
+++ src/bin/day009.rs
@@ -1,8 +1,9 @@
 use std::io::{Error, ErrorKind, Result};
 use std::str::from_utf8;
 use std::vec::Vec;
+use std::cmp::{min, max};
 
-fn part_1(nums: &Vec<usize>) -> Result<usize> {
+fn part_1(nums: &Vec<i64>) -> Result<i64> {
     let mut index = 25;
     loop {
         if index >= nums.len() {
@@ -26,16 +27,36 @@ fn part_1(nums: &Vec<usize>) -> Result<usize> {
     }
 }
 
+fn part_2(nums: &Vec<i64>, invalid_num: i64) -> i64 {
+    for k in 0..nums.len() - 1 {
+        let mut lo = i64::MAX;
+        let mut hi = i64::MIN;
+        let mut total = 0;
+        for i in &nums[k..] {
+            lo = min(lo, *i);
+            hi = max(hi, *i);
+            total += *i;
+            if total == invalid_num {
+                return lo + hi;
+            }
+        }
+    }
+    return -1;
+}
+
 fn main() {
     let input = include_bytes!("day009.input");
-    let nums: Vec<usize> = from_utf8(input)
+    let nums: Vec<i64> = from_utf8(input)
         .unwrap()
         .lines()
-        .map(|x| x.parse::<usize>().unwrap())
+        .map(|x| x.parse::<i64>().unwrap())
         .collect();
 
+    let invalid_num = part_1(&nums).unwrap();
     println!(
         "Part 1, the first invalid number: {}",
-        part_1(&nums).unwrap()
+        invalid_num
     );
+    
+    println!("Part 2, weakness number: {}", part_2(&nums, invalid_num));
 }
