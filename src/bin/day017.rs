70c7294d8782352b9c8b7ff8468ca7ebb277c61e
Lin Wang <wanglinseven@gmail.com>
2021-01-09 11:11:15 -0800

    Day 17, part 2.
    Neil's solution[1] of using a single array is better.
    
    [1]: https://github.com/sheredom/AOC2020/blob/main/src/day17/mod.rs

--- src/bin/day017.rs
+++ src/bin/day017.rs
@@ -17,27 +17,31 @@ impl fmt::Debug for CellState {
     }
 }
 
-fn clear_grid(grid: &mut Vec<Vec<Vec<CellState>>>) {
-    for y_dim in grid {
-        for z_dim in y_dim {
-            for cell in z_dim {
-                *cell = CellState::Inactive;
+fn clear_grid(grid: &mut Vec<Vec<Vec<Vec<CellState>>>>) {
+    for w_dim in grid {
+        for y_dim in w_dim {
+            for z_dim in y_dim {
+                for cell in z_dim {
+                    *cell = CellState::Inactive;
+                }
             }
         }
     }
 }
 
-fn count_active_neighbors(grid: &mut Vec<Vec<Vec<CellState>>>, z: usize, y: usize, x: usize) -> i32 {
+fn count_active_neighbors(grid: &Vec<Vec<Vec<Vec<CellState>>>>, w: usize, z: usize, y: usize, x: usize) -> i32 {
     let mut count = 0;
-    if grid[z][y][x] == CellState::Active {
+    if grid[w][z][y][x] == CellState::Active {
         count -= 1; // don't count itself.
     }
 
-    for iz in (z - 1)..(z + 2) {
-        for iy in (y - 1)..(y + 2) {
-            for ix in (x - 1)..(x + 2) {
-                if grid[iz][iy][ix] == CellState::Active {
-                    count += 1; // account for itself.
+    for iw in (w - 1)..(w + 2) {
+        for iz in (z - 1)..(z + 2) {
+            for iy in (y - 1)..(y + 2) {
+                for ix in (x - 1)..(x + 2) {
+                    if grid[iw][iz][iy][ix] == CellState::Active {
+                        count += 1; // account for itself.
+                    }
                 }
             }
         }
@@ -46,13 +50,15 @@ fn count_active_neighbors(grid: &mut Vec<Vec<Vec<CellState>>>, z: usize, y: usiz
     return count;
 }
 
-fn count_active(grid: &Vec<Vec<Vec<CellState>>>) -> i32 {
+fn count_active(grid: &Vec<Vec<Vec<Vec<CellState>>>>) -> i32 {
     let mut active_cnt = 0;
-    for y_dim in grid {
-        for z_dim in y_dim {
-            for cell in z_dim {
-                if *cell == CellState::Active {
-                    active_cnt += 1;
+    for w_dim in grid {
+        for y_dim in w_dim {
+            for z_dim in y_dim {
+                for cell in z_dim {
+                    if *cell == CellState::Active {
+                        active_cnt += 1;
+                    }
                 }
             }
         }
@@ -60,39 +66,32 @@ fn count_active(grid: &Vec<Vec<Vec<CellState>>>) -> i32 {
     return active_cnt;
 }
 
-fn part_1(grid: &Vec<Vec<Vec<CellState>>>) -> i32 {
+fn part_1(grid: &Vec<Vec<Vec<Vec<CellState>>>>) -> i32 {
     let mut grid0 = grid.clone();
     let mut grid1 = grid.clone();
 
     let grid_src = &mut grid0;
     let grid_dest = &mut grid1;
 
-    /*
-    for y_dim in &grid_src[3] {
-        println!("{:?}", y_dim);
-    }
-    println!("\n");
-    */
-
-    let search_index_end = grid_src.len() - 1;
+    let w = grid.len() / 2;
 
     for _ in 0..6 {
         mem::swap(grid_src, grid_dest); 
         clear_grid(grid_dest);
 
-        for z in 1..search_index_end {
-            for y in 1..search_index_end {
-                for x in 1..search_index_end {
-                    let active_neighbors = count_active_neighbors(grid_src, z, y, x);
-                    match grid_src[z][y][x] {
+        for z in 1..(grid_src[w].len() - 1) {
+            for y in 1..(grid_src[w][z].len() - 1) {
+                for x in 1..(grid_src[w][z][y].len() - 1) {
+                    let active_neighbors = count_active_neighbors(grid_src, w, z, y, x);
+                    match grid_src[w][z][y][x] {
                         CellState::Active => {
                             if active_neighbors == 2 || active_neighbors == 3 {
-                                grid_dest[z][y][x] = CellState::Active;
+                                grid_dest[w][z][y][x] = CellState::Active;
                             }
                         },
                         CellState::Inactive => {
                             if active_neighbors == 3 {
-                                grid_dest[z][y][x] = CellState::Active;
+                                grid_dest[w][z][y][x] = CellState::Active;
                             }
                         },
                     }
@@ -101,14 +100,42 @@ fn part_1(grid: &Vec<Vec<Vec<CellState>>>) -> i32 {
         }
     }
 
-    /*
-    for z in 2..5 {
-        for y_dim in &grid_dest[z] {
-            println!("{:?}", y_dim);
+    return count_active(grid_dest);
+}
+
+fn part_2(grid: &Vec<Vec<Vec<Vec<CellState>>>>) -> i32 {
+    let mut grid0 = grid.clone();
+    let mut grid1 = grid.clone();
+
+    let grid_src = &mut grid0;
+    let grid_dest = &mut grid1;
+
+    for _ in 0..6 {
+        mem::swap(grid_src, grid_dest); 
+        clear_grid(grid_dest);
+
+        for w in 1..(grid.len() - 1) {
+            for z in 1..(grid_src[w].len() - 1) {
+                for y in 1..(grid_src[w][z].len() - 1) {
+                    for x in 1..(grid_src[w][z][y].len() - 1) {
+                        let active_neighbors = count_active_neighbors(grid_src, w, z, y, x);
+                        match grid_src[w][z][y][x] {
+                            CellState::Active => {
+                                if active_neighbors == 2 || active_neighbors == 3 {
+                                    grid_dest[w][z][y][x] = CellState::Active;
+                                }
+                            },
+                            CellState::Inactive => {
+                                if active_neighbors == 3 {
+                                    grid_dest[w][z][y][x] = CellState::Active;
+                                }
+                            },
+                        }
+                    }
+                }
+            }
         }
-        println!("\n");
     }
-    */
 
     return count_active(grid_dest);
 }
@@ -118,35 +145,39 @@ fn main() {
     let lines = from_utf8(input).unwrap().lines();
 
     // Each cycle grows the bound by 1 on each dimension on both directions. So after 6 cycles, the
-    // bound grows 6 * 2. Adding extra one so we don't need to test out-of-bound when checking
-    // neighbors. Adding the original bound (8) results (6 + 1) * 2 + 8 = 22.
-    const DIM_MAX: usize = 22;
+    // bound grows 6 * 2 on each dimension. Adding extra one so we don't need to test out-of-bound
+    // when checking neighbors. Adding the original bound (8) results (6 + 1) * 2 + 8 = 22.
+    const CYCLES: usize = 6;
+    const W_SIZE: usize = (CYCLES + 1) * 2 + 1;
+    const Z_SIZE: usize = (CYCLES + 1) * 2 + 1;
+    const Y_SIZE: usize = (CYCLES + 1) * 2 + 8;
+    const X_SIZE: usize = (CYCLES + 1) * 2 + 8;
 
     let mut grid = Vec::new();
-    for _z in 0..DIM_MAX {
-        let mut y_dim = Vec::new();
-        for _y in 0..DIM_MAX {
-            let mut x_dim = Vec::new();
-            x_dim.resize(DIM_MAX, CellState::Inactive);
-            y_dim.push(x_dim);
+    for _w in 0..W_SIZE {
+        let mut z_dim = Vec::new();
+        for _z in 0..Z_SIZE {
+            let mut y_dim = Vec::new();
+            for _y in 0..Y_SIZE {
+                let mut x_dim = Vec::new();
+                x_dim.resize(X_SIZE, CellState::Inactive);
+                y_dim.push(x_dim);
+            }
+            z_dim.push(y_dim);
         }
-        grid.push(y_dim);
+        grid.push(z_dim);
     }
 
-    let start_pos = DIM_MAX / 2 - lines.clone().count() / 2;
-    let mut y = start_pos;
-    for line in lines {
-        let mut x = start_pos;
-        for c in line.chars() {
+    for (y, line) in lines.enumerate() {
+        for (x, c) in line.chars().enumerate() {
             match c {
-                '.' => grid[DIM_MAX / 2][y][x] = CellState::Inactive,
-                '#' => grid[DIM_MAX / 2][y][x] = CellState::Active,
+                '.' => grid[W_SIZE / 2][Z_SIZE / 2][y + (CYCLES + 1)][x + (CYCLES + 1)] = CellState::Inactive,
+                '#' => grid[W_SIZE / 2][Z_SIZE / 2][y + (CYCLES + 1)][x + (CYCLES + 1)] = CellState::Active,
                 _ => panic!("Invalid cell state."),
             }
-            x += 1;
         }
-        y += 1;
     }
 
     println!("Part 1: {}", part_1(&grid));
+    println!("Part 1: {}", part_2(&grid));
 }
