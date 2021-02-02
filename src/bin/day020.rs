a0b589e9e630660e50a4b14a167b2644a3fe7dad
Lin Wang <wanglinseven@gmail.com>
2021-02-01 17:29:52 -0800

    Refactor part 1, preparing for part 2.

--- src/bin/day020.rs
+++ src/bin/day020.rs
@@ -2,18 +2,23 @@ use std::str::from_utf8;
 use std::vec::Vec;
 use intbits::Bits;
 use bit_reverse::BitwiseReverse;
-// use bitvec::prelude::*;
+use std::cell::RefCell;
 
 const TILE_WIDTH: usize = 10;
 
 struct Tile {
+    id: u32,
     rows: [u16; TILE_WIDTH],
+    // north, west, south, east
+    adjacent: RefCell<[Option<u32>; 4]>,
 }
 
 impl Tile {
-    pub fn new(rows: [u16; TILE_WIDTH]) -> Self {
+    pub fn new(id: u32, rows: [u16; TILE_WIDTH]) -> Self {
         return Self {
-            rows
+            id,
+            rows,
+            adjacent: RefCell::new([None; 4]),
         };
     }
 
@@ -21,14 +26,31 @@ impl Tile {
         self.rows[row_index] = row;
     }
 
+    pub fn get_row(&self, row_index: usize) -> u16 {
+        return self.rows[row_index];
+    }
+
+    pub fn set_adjacent(&self, dir: usize, tile_id: u32) {
+        let mut adjacent = self.adjacent.borrow_mut();
+
+        assert_eq!(adjacent[dir], None, 
+                   "Both {:?} and {} are adjacent to {}", 
+                   adjacent[dir], tile_id, self.id);
+
+        adjacent[dir] = Some(tile_id);
+    }
+
+    pub fn adjacent_count(&self) -> usize {
+        return self.adjacent.borrow().iter().fold(0, |acc, adj| {
+            if *adj != None {
+                acc + 1
+            } else {
+                acc 
+            }
+        });
+    }
+
     pub fn get_borders(&self) -> (u16, u16, u16, u16) {
-        // south 
-        let s = self.rows[0];
-        // east 
-        let mut e: u16 = 0;
-        for (i, row) in self.rows.iter().enumerate() {
-            e.set_bit(i, row.bit(0));
-        }
         // north 
         let n = self.rows[TILE_WIDTH - 1];
         // west 
@@ -36,17 +58,18 @@ impl Tile {
         for (i, row) in self.rows.iter().enumerate() {
             w.set_bit(i, row.bit(TILE_WIDTH - 1));
         }
-
-        return (s, e, n, w);
+        // south 
+        let s = self.rows[0];
+        // east 
+        let mut e: u16 = 0;
+        for (i, row) in self.rows.iter().enumerate() {
+            e.set_bit(i, row.bit(0));
+        }
+        return (n, w, s, e);
     }
 
     pub fn get_borders_flipped(&self) -> (u16, u16, u16, u16) {
-        let (s, e, n, w) = self.get_borders();
-        let mut sf = s.swap_bits();
-        sf >>= 16 - TILE_WIDTH;
-
-        let mut ef = e.swap_bits();
-        ef >>= 16 - TILE_WIDTH;
+        let (n, w, s, e) = self.get_borders();
 
         let mut nf = n.swap_bits();
         nf >>= 16 - TILE_WIDTH;
@@ -54,7 +77,24 @@ impl Tile {
         let mut wf = w.swap_bits();
         wf >>= 16 - TILE_WIDTH;
 
-        return (sf, ef, nf, wf);
+        let mut sf = s.swap_bits();
+        sf >>= 16 - TILE_WIDTH;
+
+        let mut ef = e.swap_bits();
+        ef >>= 16 - TILE_WIDTH;
+
+        return (nf, wf, sf, ef);
+    }
+
+    pub fn flip_vertically(&mut self) {
+        for r in 0..(TILE_WIDTH / 2) {
+            self.rows.swap(r, TILE_WIDTH - 1 - r);
+        }
+        self.adjacent.borrow_mut().swap(0, 2);
+    }
+
+    pub fn rotate_counter_clockwise(&mut self) {
+        self.adjacent.borrow_mut().rotate_left(1);
     }
 }
 
@@ -62,7 +102,7 @@ fn main() {
     let input = include_bytes!("day020.input");
     let mut lines = from_utf8(input).unwrap().lines();
 
-    let mut tiles: Vec<(u32, Tile)> = Vec::new();
+    let mut tiles: Vec<Tile> = Vec::new();
     loop {
         let line = lines.next();
         if line == None {
@@ -72,7 +112,7 @@ fn main() {
         let line = line.unwrap();
         if let Some(id) = line.strip_prefix("Tile ") {
             let id = id.trim().strip_suffix(":").unwrap().parse::<u32>().unwrap();
-            let mut tile = Tile::new([0; TILE_WIDTH]);
+            let mut tile = Tile::new(id, [0; TILE_WIDTH]);
             let mut row_i = 0;
             loop {
                 let line = lines.next();
@@ -96,52 +136,43 @@ fn main() {
                 tile.set_row(row_i, row);
                 row_i += 1;
             }
-            tiles.push((id, tile));
+            tiles.push(tile);
         }
     }
 
-    let mut neighbours = Vec::new();
+    for tile in &tiles {
+        let (n, w, s, e) = tile.get_borders();
+        let borders = [n, w, s, e];
 
-    for (id, tile) in &tiles {
-        let (s, e, n, w) = tile.get_borders();
-        let (sf, ef, nf, wf) = tile.get_borders_flipped();
-        let borders = [s, e, n, w, sf, ef, nf, wf];
-        let mut neighbour_cnt = 0;
-
-        for (other_id, other_tile) in &tiles {
-            if id == other_id {
+        for other_tile in &tiles {
+            if tile.id == other_tile.id {
                 continue;
             }
 
-            let (os, oe, on, ow) = other_tile.get_borders();
-            let (osf, oef, onf, owf) = other_tile.get_borders_flipped();
-            let other_borders = [os, oe, on, ow, osf, oef, onf, owf];
+            let (on, ow, os, oe) = other_tile.get_borders();
+            let (onf, owf, osf, oef) = other_tile.get_borders_flipped();
+            let other_borders = [on, ow, os, oe, onf, owf, osf, oef];
 
-            let mut is_neighbour = false;
-            for b in borders.iter() {
+            let mut is_adjacent = false;
+            for i in 0..borders.len() {
                 for ob in other_borders.iter() {
-                    if b == ob {
-                        is_neighbour = true;
+                    if borders[i] == *ob {
+                        is_adjacent = true;
+                        tile.set_adjacent(i, other_tile.id);
                         break;
                     }
                 }
-                if is_neighbour {
+                if is_adjacent {
                     break;
                 }
             }
-
-            if is_neighbour {
-                neighbour_cnt += 1;
-            }
         }
-        neighbours.push(neighbour_cnt);
     }
 
     let mut res: usize = 1;
-    for (i, cnt) in neighbours.iter().enumerate() {
-        if *cnt == 2 {
-            let (id, _) = tiles[i];
-            res *= id as usize;
+    for tile in tiles.iter() {
+        if tile.adjacent_count() == 2 {
+            res *= tile.id as usize;
         }
     }
 
